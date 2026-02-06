4a4733be25c3dfd0dee4e222f59f138576800f94
alassane8 <alassane.wade@edu.ece.fr>
2026-02-06 20:23:32 +0100

    feat: initializing project

---
+++ src/main/java/com/example/penste/PensteApplication.java
@@ -0,0 +1,13 @@
+package com.example.penste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensteApplication.class, args);
+	}
+
+}
