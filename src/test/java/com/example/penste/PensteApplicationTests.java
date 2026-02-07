ed83bc75285d3d29df3f1edf4aa8398e6b5035d7
alassane8 <alassane.wade@edu.ece.fr>
2026-02-07 11:15:27 +0100

    chore: cicd

--- src/test/java/com/example/penste/PensteApplicationTests.java
+++ src/test/java/com/example/penste/PensteApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.penste;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PensteApplicationTests {
 
 	@Test
