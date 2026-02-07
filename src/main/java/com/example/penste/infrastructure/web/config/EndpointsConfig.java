a4962c3758b1bb79c64117fe423241b5098edfd6
alassane8 <alassane.wade@edu.ece.fr>
2026-02-07 15:46:12 +0100

    feat: updated endpoints static definition to be used in all user's controllers

--- src/main/java/com/example/penste/infrastructure/web/config/EndpointsConfig.java
+++ src/main/java/com/example/penste/infrastructure/web/config/EndpointsConfig.java
@@ -6,5 +6,5 @@ public final class EndpointsConfig {
     private EndpointsConfig() {}
 
     public static final String SERVER_API_V1 = "/server-api/v1/";
-    public static final String GET_USERS = "users";
+    public static final String USERS = "users";
 }
