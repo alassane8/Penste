5035d6f1611274e8985c4190bb6265dfaf7e0313
alassane8 <alassane.wade@edu.ece.fr>
2026-02-09 19:36:04 +0100

    feat: add function definitions to ports

--- src/main/java/com/example/penste/user/adapter/out/persistence/CreateUserPort.java
+++ src/main/java/com/example/penste/user/adapter/out/persistence/CreateUserPort.java
@@ -1,4 +1,18 @@
 package com.example.penste.user.adapter.out.persistence;
 
+import com.example.penste.user.domain.model.User;
+
+
 public interface CreateUserPort {
+
+    /** Check if user exists by email
+     * @return boolean of existing user by email
+     */
+    Boolean existsByEmail(String email);
+
+    /** Save user return the saved user
+     *  @return saved user
+     *  @param userToSave user to be saved
+     */
+    UserEntity saveUser(User userToSave);
 }
